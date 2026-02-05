cefef4727f72017dde005beabd183db20f2c323b
shahidansari311 <shahidansari945256@gmail.com>
2026-02-05 10:06:51 +0530

    added java notes

---
+++ lab/A.java
@@ -0,0 +1,15 @@
+class A{
+    int a;
+    double b;
+    String c;
+    private A(){
+        a=20;
+        b=30.0;
+        c="Shivang";
+        System.out.println(a+" "+b+" "+c);
+    } 
+    public static void main(String shivang[]) {
+        A s=new A();
+        s.close();
+    }
+} 
