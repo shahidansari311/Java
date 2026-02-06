c12c74e1cd9ce9cba8f97817867091627ee84558
shahidansari311 <shahidansari945256@gmail.com>
2026-02-06 10:17:39 +0530

    prime number using cmd line

--- lab/lab2/index3.java
+++ lab/lab2/index3.java
@@ -1,11 +1,16 @@
 public class index3 {
     public static void main(String args[]) {
         int abc = Integer.parseInt(args[0]);
-        if(abc%2==0){
-            System.out.println("Even");
+        if(abc==1){
+            System.out.printf("%d is not a prime number.",abc);
+            return;
         }
-        else{
-            System.out.println("Odd");
+        for(int i=2;i*i<=abc;i++){
+            if(abc%i==0){
+                System.out.printf("%d is not a prime number.",abc);
+                return;
+            }
         }
+        System.out.printf("%d is prime number",abc);
     }
 }
