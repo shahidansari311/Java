0d6567413fda9b94dcd43a64bec918886f56c655
shahidansari311 <shahidansari945256@gmail.com>
2026-02-06 10:05:13 +0530

    command line input

---
+++ lab/lab2/index3.java
@@ -0,0 +1,11 @@
+public class index3 {
+    public static void main(String args[]) {
+        int abc = Integer.parseInt(args[0]);
+        if(abc%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
